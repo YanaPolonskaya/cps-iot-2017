d4f6a52bfc7b98249eb271fbe182c32e5595db16
YanaPolonskaya <yanapolonskaya1413@yandex.ru>
2017-11-17 21:31:40 +0700

    811

--- ex13.js
+++ ex13.js
@@ -7,23 +7,17 @@ console.log("Starting the code");
 
 var board = new firmata.Board("/dev/ttyACM0", function(){
     console.log("Connecting to Arduino");
-    console.log("Enabling analog Pin 0");
-    board.pinMode(0, board.MODES.ANALOG); // analog pin 0
-    console.log("Enabling analog Pin 1");
-    board.pinMode(1, board.MODES.ANALOG); // analog pin 1
     
-     console.log("Activation of Pin 13");
-    console.log("Activation of Pin 12");
-    console.log("Activation of Pin 8");
-
-    board.pinMode(13, board.MODES.OUTPUT); // Configures the specified pin to behave either as an input or an output.
-    board.pinMode(12, board.MODES.OUTPUT); 
-    board.pinMode(8, board.MODES.OUTPUT); 
-
+    board.pinMode(0, board.MODES.ANALOG); // analog pin 0
+    
+    board.pinMode(1, board.MODES.ANALOG); // analog pin 1
+    board.pinMode(2, board.MODES.OUTPUT); // direction of DC motor
+board.pinMode(3, board.MODES.PWM); // PWM of motor i.e. speed of rotation
+board.pinMode(4, board.MODES.OUTPUT); // direction DC motor
 });
 
 function handler(req, res) {
-    fs.readFile(__dirname + "/assignment06.html",
+    fs.readFile(__dirname + "/example13.html",
     function (err, data) {
         if (err) {
             res.writeHead(500, {"Content-Type": "text/plain"});
@@ -36,36 +30,39 @@ function handler(req, res) {
 
 var desiredValue = 0; // desired value var
 var actualValue = 0; // variable for actual value (output value)
-
+var factor = 0.1;
+var pwm=0;
 http.listen(8080); // server will listen on port 8080
 
 board.on("ready", function() {
     
     board.analogRead(0, function(value){
-        
-        
         desiredValue = value; // continuous read of analog pin 0
-        
-       
-      
-        
-        
-        
     });
     board.analogRead(1, function(value) {
         actualValue = value; // continuous read of pin A1
-        
-        
-        
-        
     });
     
     io.sockets.on("connection", function(socket) {
         socket.emit("messageToClient", "Server connected, board ready.");
         setInterval(sendValues, 40, socket); // na 40ms we send message to client
+        startControlAlgorithm();
     }); // end of sockets.on connection
 
 }); // end board.digitalRead on pin 2
+
+function controlAlgorithm () {
+     pwm = factor*(desiredValue-actualValue);
+    if(pwm > 255) {pwm = 255}; // to limit the value for pwm / positive
+    if(pwm < -255) {pwm = -255}; // to limit the value for pwm / negative
+    if (pwm > 0) {board.digitalWrite(2,1); board.digitalWrite(4,0);}; // določimo smer če je > 0
+    if (pwm < 0) {board.digitalWrite(2,0); board.digitalWrite(4,1);}; // določimo smer če je < 0
+    board.analogWrite(3, Math.abs(pwm));
+};
+    function startControlAlgorithm () {
+    setInterval(function() {controlAlgorithm(); }, 30); // na 30ms call
+    console.log("Control algorithm started")
+};
 
 function sendValues (socket) {
     socket.emit("clientReadValues",
@@ -73,33 +70,5 @@ function sendValues (socket) {
     "desiredValue": desiredValue,
     "actualValue": actualValue
     });
-    
-      if (actualValue<desiredValue)
-        {
-           board.digitalWrite(8, board.LOW); // write LOW on pin 13 
-        }
-        
-        else {board.digitalWrite(8, board.HIGH); // write LOW on pin 13
-        }
-        
-        if (actualValue==desiredValue)
-        {
-           board.digitalWrite(12, board.LOW); // write LOW on pin 13 
-        }
-        
-        else {board.digitalWrite(12, board.HIGH); // write LOW on pin 13
-        }
-        if (actualValue>desiredValue)
-        {
-           board.digitalWrite(13, board.LOW); // write LOW on pin 13 
-        }
-        
-        else {board.digitalWrite(13, board.HIGH); // write LOW on pin 13
-        }
-        
-        
-        
-    
-    
     
 };
